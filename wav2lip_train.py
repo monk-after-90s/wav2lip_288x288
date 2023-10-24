663def712643cfc9c63ea817a8e2b113b467d2ea
90houlaoheshang <907333918@qq.com>
2023-10-24 13:25:13 +0800

    训练时的评估更加频繁

--- wav2lip_train.py
+++ wav2lip_train.py
@@ -260,7 +260,8 @@ def train(device, model, train_data_loader, test_data_loader, optimizer,
         
 
 def eval_model(test_data_loader, global_step, device, model, checkpoint_dir):
-    eval_steps = 700
+    eval_steps = 100
+    # eval_steps = 700
     print('Evaluating for {} steps'.format(eval_steps))
     sync_losses, recon_losses = [], []
     step = 0
